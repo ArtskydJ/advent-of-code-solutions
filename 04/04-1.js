ecf917781ca458355627cb2514b675ede7cdc2f9
Joseph Dykstra <josephdykstra@gmail.com>
2015-12-03 23:17:10 -0600

    Add solutions for day 4

--- 04/04-1.js
+++ 04/04-1.js
@@ -1,2 +1,7 @@
+var crypto = require('crypto')
+
 module.exports = function (input) {
+	for (var i = 0; true; i++) {
+		if (crypto.createHash('md5').update(input + i).digest('hex').slice(0, 5) === '00000') return i
+	}
 }
--- 04/04-2.js
+++ 04/04-2.js
@@ -1,2 +1,7 @@
+var crypto = require('crypto')
+
 module.exports = function (input) {
+	for (var i = 0; true; i++) {
+		if (crypto.createHash('md5').update(input + i).digest('hex').slice(0, 6) === '000000') return i
+	}
 }
