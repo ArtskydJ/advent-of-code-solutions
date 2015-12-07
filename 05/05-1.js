699a9ac5ea47660ea584466762145c5e724bed02
Joseph Dykstra <josephdykstra@gmail.com>
2015-12-07 14:27:26 -0600

    Add solution for day 5

--- 05/05-1.js
+++ 05/05-1.js
@@ -1,2 +1,11 @@
+var sum = require('../lib/sum.js')
+
 module.exports = function (input) {
+	return sum(input.split('\n').map(isNice))
+}
+
+function isNice(str) {
+	return /([aeiou].*){3}/.test(str) &&
+		/(.)\1/.test(str) &&
+		!/ab|cd|pq|xy/.test(str)
 }
--- 05/05-2.js
+++ 05/05-2.js
@@ -1,2 +1,9 @@
+var sum = require('../lib/sum.js')
+
 module.exports = function (input) {
+	return sum(input.split('\n').map(isNice))
+}
+
+function isNice(str) {
+	return /(..).*\1/.test(str) && /(.).\1/.test(str)
 }
