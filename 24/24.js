f5fdb3054e9edf3aaca47c51b065913ef2139d80
Joseph Dykstra <josephdykstra@gmail.com>
2016-05-21 12:30:09 -0500

    Add day 24

--- 24/24.js
+++ 24/24.js
@@ -1,58 +1,43 @@
 var sum = require('../lib/sum.js')
-var combinations = require('../lib/combinations.js')
-// var getQuantumEntanglement = require('../lib/multiply.js')
+var multiply = require('../lib/multiply.js')
 
-/*
-.reduce(function flatten(arr1, arr2) {
-	return arr1.concat(arr2)
-}, [])
-*/
+function iterate(k, usedIndexes) {
+	var currentPackages = usedIndexes.map(i => k.arr[i])
+	var currentSum = sum(currentPackages)
+	var currentQe = multiply(currentPackages)
 
-function iterate(soFar, available, numOfGroups) {
-	var groupSize = sum(available) / numOfGroups
-	console.log(groupSize)
-	var combos = combinations(available.length, 2)
-	console.log(combos)
+	var lastIndex = usedIndexes[usedIndexes.length - 1]
+	var nextIndex = typeof lastIndex === 'number' ? lastIndex + 1 : 0
 
-	var groups = combos.map(function (combo) {
-		var oks = combo.split('').map(Number)
-		return available.filter(function (_, i) {
-			return oks[i]
-		})
-	}).filter(function (list) {
-		return sum(list) === groupSize
-	}).map(function (group) {
-		console.log('group', group)
-		var remaining = available.filter(function (x) {
-			return group.indexOf(x) === -1
-		})
-		console.log('remaining', remaining)
-		console.log('so far', soFar.concat([group]))
-		if (numOfGroups >= 2) {
-			if (remaining.length) {
-				return iterate(soFar.concat([group]), remaining, numOfGroups - 1)
-			} else {
-				return '-'
-			}
-		} else {
-			return soFar.concat([group]) // last group
+	var shouldKeepIterating = usedIndexes.length + 1 < k.maxDepth
+
+	var smallestQe = Infinity
+
+	for (var j = nextIndex; j < k.arr.length; j++) {
+		var thisSum = currentSum + k.arr[j]
+		var thisQe = Infinity
+
+		if (shouldKeepIterating && thisSum < k.targetSum) { // KEEP ITERATING and sum is small enough
+			thisQe = iterate(k, usedIndexes.concat(j))
+		} else if (!shouldKeepIterating && thisSum === k.targetSum) { // LEAF NODE and valid sum
+			thisQe = currentQe * k.arr[j]
 		}
-	})/*.reduce(function flatten(memo, arr) {
-		for (var i = 0; i < arr.length; i++) {
-			memo.push(arr[i])
-		}
-		return memo
-	}, [])*/
-	return groups
+
+		if (thisQe < smallestQe) smallestQe = thisQe
+	}
+
+	return smallestQe
 }
 
-module.exports = function (input, half) {
-	// input = input.map(Number)
-	input = [ 1, 2, 3, 4, 5, 7, 8, 9, 10, 11 ]
-	//input = [ 1, 2, 4, 5 ]
 
-	var groups = iterate([], input, /*3*/ 3)
-	console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
-	console.log(require('util').inspect(groups, { depth: 5 }))
-	return null // groups
+module.exports = function (input, half) {
+	input = input.map(Number)
+	//input = [ 1, 2, 3, 4, 5, 7, 8, 9, 10, 11 ]
+	var targetSum = sum(input) / (3 + half) // 3 or 4
+
+	var smallestQe = Infinity
+	for (var allowDepth = 1; smallestQe === Infinity; allowDepth++) {
+		smallestQe = iterate({ arr: input, maxDepth: allowDepth, targetSum: targetSum }, [])
+	}
+	return smallestQe
 }
