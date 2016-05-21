795637c79a96fa5124ac44cc50fb287a2af09c7e
Joseph Dykstra <josephdykstra@gmail.com>
2016-05-21 12:29:04 -0500

    Add runtime output

--- aoc.js
+++ aoc.js
@@ -4,22 +4,21 @@ var path = require('path')
 var day = Number(process.argv[2])
 var half = Number(process.argv[3])
 
-console.log(runSolution(day, half))
-
-function runSolution(day, half) {
-	if (!day || !half) printUsageAndExit()
-	day = '/' + ('0' + day).slice(-2)
-	var fn = null
-	try {
-		fn = require('.' + day + day + '-' + half)
-	} catch (err) {
-		if (err.toString().slice(0, 25) !== 'Error: Cannot find module') throw err
-		fn = require('.' + day + day)
-	}
-	var input = fs.readFileSync(path.join(__dirname, day, day + '-input.txt'), 'utf8')
-	input = /\n/.test(input) ? input.split(/\r?\n/g) : input
-	return fn(input, half - 1)
+if (!day || !half) printUsageAndExit()
+day = '/' + ('0' + day).slice(-2)
+var fn = null
+try {
+	fn = require('.' + day + day + '-' + half)
+} catch (err) {
+	if (err.toString().slice(0, 25) !== 'Error: Cannot find module') throw err
+	fn = require('.' + day + day)
 }
+var input = fs.readFileSync(path.join(__dirname, day, day + '-input.txt'), 'utf8')
+input = /\n/.test(input) ? input.split(/\r?\n/g) : input
+
+console.time('Run time')
+console.log(fn(input, half - 1))
+console.timeEnd('Run time')
 
 function printUsageAndExit() {
 	console.error('Usage: node aoc <day> <half>')
