87095614a88f13940ac618094a08ae64991889ca
Joseph Dykstra <josephdykstra@gmail.com>
2016-12-30 11:09:56 -0600

    Day 2

--- 2016/02/02-1.js
+++ 2016/02/02-1.js
@@ -1,3 +1,10 @@
 module.exports = function (input) {
-	return input
+	var x = 1, y = 1
+	return input.map(function (line) {
+		line.split('').forEach(function (dir) {
+			x = Math.min(2, Math.max(0, x + ({ L: -1, R: 1 }[dir] || 0)))
+			y = Math.min(2, Math.max(0, y + ({ U: -1, D: 1 }[dir] || 0)))
+		})
+		return [[ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ]][y][x]
+	}).join('')
 }
--- 2016/02/02-2.js
+++ 2016/02/02-2.js
@@ -1,3 +1,16 @@
 module.exports = function (input) {
-	return input
+	var x = 0, y = 2
+	return input.map(function (line) {
+		line.split('').forEach(function (dir) {
+			x = Math.min(4 - Math.abs(y - 2), Math.max(Math.abs(y - 2), x + ({ L: -1, R: 1 }[dir] || 0)))
+			y = Math.min(4 - Math.abs(x - 2), Math.max(Math.abs(x - 2), y + ({ U: -1, D: 1 }[dir] || 0)))
+		})
+		return [
+			[ ' ', ' ', '1', ' ', ' ' ],
+			[ ' ', '2', '3', '4', ' ' ],
+			[ '5', '6', '7', '8', '9' ],
+			[ ' ', 'A', 'B', 'C', ' ' ],
+			[ ' ', ' ', 'D', ' ', ' ' ]
+		][y][x]
+	}).join('')
 }
