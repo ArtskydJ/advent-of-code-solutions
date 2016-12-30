fd029f61dc5664374dd21e93f0e4610e674ac9e3
Joseph Dykstra <josephdykstra@gmail.com>
2016-12-30 10:43:13 -0600

    Day 1

--- 2016/01/01-1.js
+++ 2016/01/01-1.js
@@ -1,3 +1,11 @@
 module.exports = function (input) {
-	return input
+	var rays = input.split(', ').reduce(function (memo, curr) {
+		var turn = { L: -1, R: 1}[curr[0]]
+		var nowHeading = (memo.heading + 4 + turn) % 4
+		var distance = Number(curr.slice(1))
+		memo[nowHeading] += distance
+		memo.heading = nowHeading
+		return memo
+	}, { 0: 0, 1: 0, 2: 0, 3: 0, heading: 0 })
+	return Math.abs(rays[0] - rays[2]) + Math.abs(rays[1] - rays[3])
 }
--- 2016/01/01-2.js
+++ 2016/01/01-2.js
@@ -1,3 +1,19 @@
 module.exports = function (input) {
-	return input
+	return input.split(', ').reduce(function (memo, curr) {
+		var turn = { L: -1, R: 1 }[curr[0]]
+		memo.heading = (memo.heading + 4 + turn) % 4
+		var distance = Number(curr.slice(1))
+
+		for (var i = 1; i <= distance; i++) {
+			memo.x += [ 0, 1, 0, -1 ][memo.heading]
+			memo.y += [ -1, 0, 1, 0 ][memo.heading]
+			var location = memo.x + ',' + memo.y
+			if (memo.visited.has(location) && !memo.firstVisitedTwiceDistance) {
+				memo.firstVisitedTwiceDistance = Math.abs(memo.x) + Math.abs(memo.y)
+			}
+			memo.visited.add(location)
+			//console.log(location)
+		}
+		return memo
+	}, { visited: new Set(), heading: 0, x: 0, y: 0, firstVisitedTwiceDistance: '' }).firstVisitedTwiceDistance
 }
