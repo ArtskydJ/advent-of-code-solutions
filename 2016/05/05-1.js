1b59e4e2198e5b344dbf318223508c31b4674627
Joseph Dykstra <josephdykstra@gmail.com>
2017-01-02 09:45:33 -0600

    Day 5

--- 2016/05/05-1.js
+++ 2016/05/05-1.js
@@ -1,3 +1,14 @@
+var crypto = require('crypto')
+
 module.exports = function (input) {
-	return input
+	var password = ''
+	for (var i = 0; true; i++) {
+		var md5 = crypto.createHash('md5')
+		md5.update(input + i)
+		var hash = md5.digest('hex')
+		if (hash[0] === '0' && hash[1] === '0' && hash[2] === '0' && hash[3] === '0' && hash[4] === '0') {
+			password += hash[5]
+			if (password.length >= 8) return password
+		}
+	}
 }
--- 2016/05/05-2.js
+++ 2016/05/05-2.js
@@ -1,3 +1,17 @@
+var crypto = require('crypto')
+
 module.exports = function (input) {
-	return input
+	var password = '________'.split('')
+	for (var i = 0; true; i++) {
+		var md5 = crypto.createHash('md5')
+		md5.update(input + i)
+		var hash = md5.digest('hex')
+		if (hash[0] === '0' && hash[1] === '0' && hash[2] === '0' && hash[3] === '0' && hash[4] === '0') {
+			var index = Number(hash[5])
+			if (index < 8 && password[index] === '_') {
+				password[index] = hash[6]
+			}
+			if (password.indexOf('_') === -1) return password.join('')
+		}
+	}
 }
